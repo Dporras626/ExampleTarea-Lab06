6d20478851db266732211ca0604c1ecfb976fd9d
diegoporras <diegoporras@gmail.com>
2023-05-07 20:42:59 -0500

    Agregar archivos de proyecto.

---
+++ ExampleTarea/ExampleTarea/ListViewGroup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExampleTarea
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewGroup : ContentPage
+    {
+        public ObservableCollection<Group> Groups { get; set; }
+        public ListViewGroup()
+        {
+            InitializeComponent();
+            Groups = new ObservableCollection<Group>
+        {
+            new Group("A")
+            {
+                new Item { Title = "Real", Subtitle = "Madrid" },
+                new Item { Title = "Atletico", Subtitle = "Madrid" },
+                new Item { Title = "Real", Subtitle = "Betis" },
+                new Item { Title = "Alianza", Subtitle = "Lima" }
+            },
+            new Group("B")
+            {
+                new Item { Title = "Diego", Subtitle = "Porras" },
+                new Item { Title = "Sideral", Subtitle = "Carrion" },
+                new Item { Title = "Rafael", Subtitle = "Cardozo" },
+                new Item { Title = "Leao", Subtitle = "Butron" }
+            },
+            new Group("C")
+            {
+                new Item { Title = "Lionel", Subtitle = "Messi" },
+                new Item { Title = "Cuto", Subtitle = "Guadalupe" },
+                new Item { Title = "Reymo", Subtitle = "Mance" },
+                new Item { Title = "Carlos", Subtitle = "Saravia" }
+            },
+        };
+            GroupedView.ItemsSource = Groups;
+        }
+    }
+}
---
+++ ExampleTarea/ExampleTarea/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ExampleTarea
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Item15.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ListViewGroup());
+            };
+
+        }
+    }
+}
